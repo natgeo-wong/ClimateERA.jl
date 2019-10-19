a7b183cc23220efc75efac2941d9c3fd9288e349
Nathanael Wong <natgeo.wong@outlook.com>
2019-10-18 21:09:55 -0400

    Basic functionality for eratime.  More complicated stuff to come later.

--- src/eratime.jl
+++ src/eratime.jl
@@ -3,10 +3,13 @@ Temporary file for setting up of ClimateERA time modules before shoving everythi
 erainitialize module.
 
 """
-<<<<<<< HEAD
 
-function eratime(init)
-    
+function eratime(timeID::Int64)
+    if timeID == 0, return Dict("Begin"=>1979,"End"=>Dates.year(Dates.now())-1);
+    else            return Dict("Begin"=>timeID,"End"=>timeID)
+    end
 end
-=======
->>>>>>> f6910db1b6aa1c635839ccad70c28ecad62674ce
+
+function eratime(init,timeID::Array)
+    return Dict("Begin"=>minimum(timeID),"End"=>maximum(timeID))
+end
