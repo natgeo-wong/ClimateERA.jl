7502a4d9afc6488e2cd0d0e61aa66a629c099b0f
Nathanael Wong <natgeo.wong@outlook.com>
2019-11-28 20:49:47 -0500

    Force move file.

--- src/download.jl
+++ src/download.jl
@@ -200,7 +200,7 @@ function eratmp2raw(efol::Dict)
 
     if lf > 0
         @info "$(Dates.now()) - Moving ERA reanalysis data from tmp to raw folder."
-        for ii = 1 : lf; mv(fnc[ii],joinpath(efol["raw"],fnc[ii])); end
+        for ii = 1 : lf; mv(fnc[ii],joinpath(efol["raw"],fnc[ii]),,force=true); end
         @info "$(Dates.now()) - Downloaded ERA reanalysis data has been moved raw folder."
     else
         @info "$(Dates.now()) - ERA reanalysis tmp folder is empty.  Nothing to shift."
