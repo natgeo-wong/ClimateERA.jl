3aa3d6570f9a08e5b18384d8d98aee752e4b61f3
Nathanael Wong <natgeo.wong@outlook.com>
2020-03-12 15:52:15 -0400

    Added more backend functions for the printing of dates to strings.

--- src/backend.jl
+++ src/backend.jl
@@ -4,3 +4,8 @@ yrmo2dir(date::TimeType) = Dates.format(date,dateformat"yyyy/mm")
 yrmo2str(date::TimeType) = Dates.format(date,dateformat"yyyymm")
 yr2str(date::TimeType)   = Dates.format(date,dateformat"yyyy")
 ymd2str(date::TimeType)  = Dates.format(date,dateformat"yyyymmdd")
+mo2str(date::TimeType)   = Dates.format(date,dateformat"mm")
+
+yrmo2str(yr::Integer,mo::Integer) = @sprintf("%04d%02d",yr,mo)
+mo2str(mo::Integer) = @sprintf("%02d",mo)
+dy2str(dy::Integer) = @sprintf("%02d",dy)
