c25c207c04d1d2113f2b4a285fa7c48a2986c3ae
Nathanael Wong <natgeo.wong@outlook.com>
2020-03-14 17:14:05 -0400

    Added query functions to ClimateERA.jl functionalities.

--- src/ClimateERA.jl
+++ src/ClimateERA.jl
@@ -7,19 +7,23 @@ module ClimateERA
 using Logging, Dates
 using DelimitedFiles, Printf, Statistics
 using NCDatasets, Glob, JLD2, FileIO
+using Crayons, PrettyTables
 using GeoRegions
 
 ## Exporting the following functions:
 export
         eramodule, eraparameters, erapressure, eratime, eraregion,
         erastartup, erainitialize, eroot, erancread,
-        erafolder, eratmp2raw, eradownload, eraanalysis
+        erafolder, eratmp2raw, eradownload, eraanalysis,
+        queryeaction, queryedataset, queryemodule, queryparameter
 
 ## Including other files in the module
 
 include("startup.jl")
 include("frontend.jl")
 include("backend.jl")
+include("query.jl")
+
 include("initialize.jl")
 include("download.jl")
 include("analysis.jl")
--- src/query.jl
+++ src/query.jl
@@ -1,15 +1,53 @@
 function queryeaction()
 
+    @info "$(Dates.now()) - The following actions are possible in ClimateERA.jl ..."
+    act = [1 "Download" ; 2 "Analysis"]; head = ["aID","Action"];
+    pretty_table(act,head;alignment=:c)
+
 end
 
 function queryedataset()
 
+    @info "$(Dates.now()) - The following ECMWF datasets can be manipulated with ClimateERA.jl ..."
+    dset = [1 "ERA5" ; 2 "ERA-Interim"]; head = ["dID","Dataset"];
+    pretty_table(dset,head;alignment=:c)
+
 end
 
-function queryemodule(modID::Integer)
+function queryemodule()
+
+    @info "$(Dates.now()) - The following modules are available in ClimateERA.jl ..."
+    mset = [ "dsfc" "."^10 "dry" "surface";
+             "dpre" "."^10 "dry" "pressure";
+             "msfc" "."^10 "moist / water" "surface" ;
+             "mpre" "."^10 "moist / water" "pressure" ;
+             "csfc" "."^10 "calculated" "surface" ;
+             "cpre" "."^10 "calculated" "pressure" ];
+    head = ["modID","","variable type","level(s)"];
+    pretty_table(mset,head;tf=borderless,alignment=:c)
 
 end
 
-function queryemodule(modID::AbstractString)
+function queryemodule(moduleID::AbstractString)
+
+    @info "$(Dates.now()) - The following modules are available in ClimateERA.jl ..."
+    mset = [ "dsfc" "dry" "surface";
+             "dpre" "dry" "pressure";
+             "msfc" "moist / water" "surface" ;
+             "mpre" "moist / water" "pressure" ;
+             "csfc" "calculated" "surface" ;
+             "cpre" "calculated" "pressure" ];
+    head = ["modID","variable type","level(s)"];
+    pretty_table(mset,head;alignment=:c)
+
+end
+
+function queryeparameter(parameterID::AbstractString)
+
+    allparams = readdlm(joinpath(@__DIR__,"eraparameters.txt"),',',comments=true);
+
+end
+
+function queryeparameter(parameterID::AbstractString, moduleID::AbstractString)
 
 end
